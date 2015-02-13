7903242e4a1b0b1c656ef267375a4a2c93dbb772
TheLittleBoy <mac@MAC.local>
2015-02-13 13:11:47 +0800

    init

---
+++ HXAttributedString/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXAttributedString
+//
+//  Created by  MAC on 15/2/13.
+//  Copyright (c) 2015年 华夏大地教育. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HXAttributedString/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HXAttributedString
+//
+//  Created by  MAC on 15/2/13.
+//  Copyright (c) 2015年 华夏大地教育. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
